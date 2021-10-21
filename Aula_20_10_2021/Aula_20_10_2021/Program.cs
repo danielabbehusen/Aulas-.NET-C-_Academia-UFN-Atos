77dbd42e6eb6c51934e73c90b8d7ae14c57000f7
Daniel Cruz <daniel.cruz@ledax.com>
2021-10-20 23:23:40 -0300

    terceiro commit

--- Aula_20_10_2021/Aula_20_10_2021/Program.cs
+++ Aula_20_10_2021/Aula_20_10_2021/Program.cs
@@ -9,6 +9,11 @@ namespace Aula_20_10_2021
 
 
             // ex07
+            //Para doar sangue é necessário ter entre 18 e 67 anos.Faça um aplicativo que pergunte a idade de uma pessoa e diga se ela pode doar sangue ou não.Use alguns dos 
+            //    operadores lógicos OU(||) e E(&&).
+
+
+
             //int idade;
 
             //Console.Write("Digite a sua idade: ");
@@ -22,9 +27,6 @@ namespace Aula_20_10_2021
             //{
             //    Console.WriteLine("Não pode doar sangue");
             //}
-
-
-
 
 
 
