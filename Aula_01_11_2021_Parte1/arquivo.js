200eeeec571711a5e0732314f27ee56390b2c326
Daniel Cruz <daniel.cruz@ledax.com>
2021-11-01 20:31:41 -0300

    aula 1 parte

---
+++ Aula_01_11_2021_Parte1/arquivo.js
@@ -0,0 +1,49 @@
+// var numero = 1;
+
+// if (numero == "1") {
+//     alert('True')
+// } else {
+//     alert('False')
+// }
+
+
+// if (numero === "1") {
+//     alert('True')
+// } else {
+//     alert('False')
+// }
+
+
+// var suco = 3;
+
+// switch (suco) {
+//     case 1:
+//         alert('fazer 1 copo de suco')
+//         break
+//     case 2:
+//         alert('fazer meia jarra de suco')
+//         break
+//     case 3:
+//         alert('fazer uma jarra de suco')
+//         break
+//     default:
+//         alert('opção default')
+// }
+
+// var nomes = "";
+// var alunos = ["Rodrigo", "Eduardo", "thiago", "Pablo", "Francisco"];
+
+// for (let i = 0; i < alunos.length; i++) {
+//     alert(alunos[i])
+// }
+
+
+// var texto = "";
+// var numero = 1;
+
+// while (numero < 10) {
+//     texto += numero + " ";
+//     numero++;
+// }
+
+// alert(texto)
